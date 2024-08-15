04b7256a6b77cf9d3f6dd547acc4421e3131f0d7
Hamabi_minami <1220625658@qq.com>
2024-08-14 21:19:57 -0600

    new file:   Components/Pages/BookManagement.razor 	new file:   Models/Book.cs 	new file:   Models/Student.cs 	new file:   ViewModels/BookViewModel.cs 	new file:   library.db

---
+++ libraryManagement/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace libraryManagement.Models
+{
+    [SQLite.Table("Books")]
+    public class Book
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(255)]
+        public string Title { get; set; }
+        [MaxLength(255)]
+        public string Author { get; set; }
+        [MaxLength(255)]
+        public string Publisher { get; set; }
+        public DateTime PublishDate { get; set; }
+        [MaxLength(13)]
+        public string ISBN { get; set; }
+        public int Stock { get; set; }
+    }
+}
+
---
+++ libraryManagement/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace libraryManagement.Models
+{
+    [SQLite.Table("Students")]
+    public class Student
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+        [MaxLength(100)]
+        public string LastName { get; set; }
+        [MaxLength(15)]
+        public string Phone { get; set; }
+        [MaxLength(100)]
+        public string Email { get; set; }
+    }
+}
+
